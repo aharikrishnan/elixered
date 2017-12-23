cf6b3f3e454ad5d42ac9c343e96d179fd5544f8b
harikrishnan <harikrishnan.a@infibeam.net>
2017-12-23 17:51:02 +0530

    changing port and adding templates to static

--- lib/red_web/endpoint.ex
+++ lib/red_web/endpoint.ex
@@ -9,7 +9,7 @@ defmodule RedWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :red, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js templates favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
